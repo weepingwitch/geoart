b7cbea140ad8d406d06cf7053909489903a52109
weepingwitch <willowcarter42@gmail.com>
2016-05-08 23:06:47 -0400

    updating island generation code

--- rndmap.php
+++ rndmap.php
@@ -14,89 +14,35 @@ for($Row = 1; $Row <= $Height; $Row++) {
     }
 }
 
-$x = 16;
-$y = 16;
-for($c=0; $c <= mt_rand(45,95); $c++){
-    $x = $x - 2 + mt_rand(1,3);
-    $y = $y - 2 + mt_rand(1,3);
-    $land = imagecolorallocate($Image,mt_rand(200,205),mt_rand(150,200),mt_rand(100,105));
-    imagesetpixel($Image,$x,$y,$land);
+//island paths branching from the center
+for ($p=0; $p <= mt_rand(6,12); $p++){
+    //start at the center
+    $x = 16;
+    $y = 16;
+    //take a random number of "steps"
+    for($c=0; $c <= mt_rand(45,95); $c++){
+        //each step, randomly move from current square
+        $x = $x - 2 + mt_rand(1,3);
+        $y = $y - 2 + mt_rand(1,3);
+        //turn new square into land
+        $land = imagecolorallocate($Image,mt_rand(200,205),mt_rand(150,200),mt_rand(100,105));
+        imagesetpixel($Image,$x,$y,$land);
 
+    }
 }
-$x = 16;
-$y = 16;
-for($c=0; $c <= mt_rand(45,95); $c++){
-    $x = $x - 2 + mt_rand(1,3);
-    $y = $y - 2 + mt_rand(1,3);
-    $land = imagecolorallocate($Image,mt_rand(200,205),mt_rand(150,200),mt_rand(100,105));
-    imagesetpixel($Image,$x,$y,$land);
+// ~random~ small island parts, doing the same thing but from random position
+for ($p=0;$p<=mt_rand(2,5);$p++){
+    $x = 16;
+    $y = 16;
+    for($c=0; $c <= mt_rand(45,95); $c++){
+        $x = $x - 2 + mt_rand(1,3);
+        $y = $y - 2 + mt_rand(1,3);
+        $land = imagecolorallocate($Image,mt_rand(200,205),mt_rand(150,200),mt_rand(100,105));
+        imagesetpixel($Image,$x,$y,$land);
 
-}
-$x = 16;
-$y = 16;
-for($c=0; $c <= mt_rand(45,95); $c++){
-    $x = $x - 2 + mt_rand(1,3);
-    $y = $y - 2 + mt_rand(1,3);
-    $land = imagecolorallocate($Image,mt_rand(200,205),mt_rand(150,200),mt_rand(100,105));
-    imagesetpixel($Image,$x,$y,$land);
-
-}
-$x = 16;
-$y = 16;
-for($c=0; $c <= mt_rand(45,95); $c++){
-    $x = $x - 2 + mt_rand(1,3);
-    $y = $y - 2 + mt_rand(1,3);
-    $land = imagecolorallocate($Image,mt_rand(200,205),mt_rand(150,200),mt_rand(100,105));
-    imagesetpixel($Image,$x,$y,$land);
-
-}
-$x = 16;
-$y = 16;
-for($c=0; $c <= mt_rand(45,95); $c++){
-    $x = $x - 2 + mt_rand(1,3);
-    $y = $y - 2 + mt_rand(1,3);
-    $land = imagecolorallocate($Image,mt_rand(200,205),mt_rand(150,200),mt_rand(100,105));
-    imagesetpixel($Image,$x,$y,$land);
-
-}
-$x = 16;
-$y = 16;
-for($c=0; $c <= mt_rand(45,95); $c++){
-    $x = $x - 2 + mt_rand(1,3);
-    $y = $y - 2 + mt_rand(1,3);
-    $land = imagecolorallocate($Image,mt_rand(200,205),mt_rand(150,200),mt_rand(100,105));
-    imagesetpixel($Image,$x,$y,$land);
-
+    }
 }
 
-$x = mt_rand(1,31);
-$y = mt_rand(1,31);
-for($c=0; $c <= mt_rand(15,55); $c++){
-    $x = $x - 2 + mt_rand(1,3);
-    $y = $y - 2 + mt_rand(1,3);
-    $land = imagecolorallocate($Image,mt_rand(200,205),mt_rand(150,200),mt_rand(100,105));
-    imagesetpixel($Image,$x,$y,$land);
-
-}
-
-$x = mt_rand(1,31);
-$y = mt_rand(1,31);
-for($c=0; $c <= mt_rand(15,55); $c++){
-    $x = $x - 2 + mt_rand(1,3);
-    $y = $y - 2 + mt_rand(1,3);
-    $land = imagecolorallocate($Image,mt_rand(200,205),mt_rand(150,200),mt_rand(100,105));
-    imagesetpixel($Image,$x,$y,$land);
-
-}
-$x = mt_rand(1,31);
-$y = mt_rand(1,31);
-for($c=0; $c <= mt_rand(15,55); $c++){
-    $x = $x - 2 + mt_rand(1,3);
-    $y = $y - 2 + mt_rand(1,3);
-    $land = imagecolorallocate($Image,mt_rand(200,205),mt_rand(150,200),mt_rand(100,105));
-    imagesetpixel($Image,$x,$y,$land);
-
-}
 
 
 
